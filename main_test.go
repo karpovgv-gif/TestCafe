10159bf73988c195c17425b93a4c2f30dfd1a61b
Карпов Григорий <karpov.gv@mail.ru>
2026-05-04 20:14:55 +0300

    first-iteration

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -45,6 +46,75 @@ func TestCafeWhenOk(t *testing.T) {
 
 		handler.ServeHTTP(response, req)
 
+		assert.Equal(t, http.StatusOK, response.Code)
+	}
+}
+
+func TestCofeCount(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+
+	requests := []struct {
+		count int //передаваемое значение
+		want  int //ожидаемое колличество кафе в ответе
+	}{
+		{0, 0},
+		{1, 1},
+		{2, 2},
+		{100, len(cafeList["moscow"])},
+	}
+	for _, v := range requests {
+		response := httptest.NewRecorder()
+		str := fmt.Sprintf("/cafe?city=moscow&count=%d", v.count)
+		req := httptest.NewRequest("GET", str, nil)
+
+		handler.ServeHTTP(response, req)
+
+		body := response.Body.String()
+
+		//fmt.Println("body: ", body)
+
+		parts := strings.Split(body, ",")
+
+		cafeCount := 0
+		for _, part := range parts {
+			if strings.TrimSpace(part) != "" {
+				cafeCount++
+			}
+		}
+
+		assert.Equal(t, v.want, cafeCount)
+	}
+}
+
+func TestCafeSearch(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+
+	requests := []struct {
+		search    string
+		wantCount int
+	}{
+		{"search=фасоль", 0},
+		{"search=кофе", 2},
+		{"search=вилка", 1},
+	}
+
+	for _, v := range requests {
+		response := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", fmt.Sprintf("/cafe?city=moscow&%s", v.search), nil)
+
+		handler.ServeHTTP(response, req)
+		body := response.Body.String()
+		fmt.Println("body ", body)
+
+		parts := strings.Split(body, ",")
+		count := 0
+		for _, part := range parts {
+			if strings.TrimSpace(part) != "" {
+				count++
+			}
+		}
+
+		assert.Equal(t, v.wantCount, count)
 		assert.Equal(t, http.StatusOK, response.Code)
 	}
 }
